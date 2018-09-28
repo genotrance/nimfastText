a857e9bebca64802658723b788a914e955096744
zhoupeng <zhoupeng@xjgreat.com>
2018-09-29 05:50:29 +0800

    fix pair def, add second version predict

--- tests/testfast.nim
+++ tests/testfast.nim
@@ -8,9 +8,11 @@ var
     print_prob = false
     k:int32 = 1
     threshold:real = 0.0
-    # output:vect[pair[real, std_string]]
-    ss = constructStdIStringStream(constructStdString("The first line"))
+    output:vect[pair[real, std_string]]
+    ss = constructStdIStringStream(constructStdString("അമ്മ"))
 
 ft.predict(ss,k,print_prob,threshold)
 
-# ft.predict(ss,k,output,threshold)
+ft.predict(ss,k,output,threshold)
+
+echo output[0]
