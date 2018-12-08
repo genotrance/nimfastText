a46949622fcbfca9dbe1f290375b1ff20d31ccc3
Ganesh Viswanathan <dev@genotrance.com>
2018-12-07 22:18:11 -0600

    Handle upstream changes

--- tests/tnimfastText.nim
+++ tests/tnimfastText.nim
@@ -4,15 +4,16 @@ var ft: FastText
 
 ft.loadModel(constructStdString("tests/lid.176.ftz"))
 
-var 
+var
     print_prob = false
     k:int32 = 1
     threshold:cfloat = 0.0
-    output:vect[pair[cfloat, std_string]]
+    words: vect[int32]
+    output:vect[pair[cfloat, int32]]
     ss = constructStdIStringStream(constructStdString("അമ്മ"))
 
-ft.predict(ss,k,print_prob,threshold)
+#ft.predict(ss,k,print_prob,threshold)
 
-#ft.predict(ss,k,output,threshold)
+ft.predict(k,words,output,threshold)
 
 #echo output[0]
